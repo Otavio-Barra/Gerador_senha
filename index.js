994b30f7e3f6e509a6506835cb9cdcaf0c9db09c
Otavio-Barra <otaviombarra@gmail.com>
2024-03-25 21:32:53 -0300

    feito js e css

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-// console.log(Math.random());
-// console.log(Math.random().toString(36));
+const checkToSymbols = document.querySelector("#symbols");
+const checkToNumber = document.querySelector("#number");
+const password = document.querySelector(".password");
+const inputLimitedCaracteres = document.querySelector(".number-caracters");
+const btnCreatePass = document.querySelector(".created-password");
+const btnCopyPass = document.querySelector(".copy-password");
 
-let simbols = "~`!@#$%^&*()_-+={[}],|:;<>.?/";
+btnCreatePass.addEventListener("click", createdPassword);
+btnCopyPass.addEventListener("click", copyPassword);
+
+let symbols = "~`!@#$%^&*()_-+={[}],|:;<>.?/";
 let latter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let number = "1234567890";
-function confereCaracteres() {
-  if (simbols && number) {
-    return latter + number;
+
+function checkCaracters() {
+  if (checkToSymbols.checked && checkToNumber.checked) {
+    return symbols + number + latter;
   }
-}
-function gerarSenha() {
-  let pass = "";
-  for (let i = 0; i < 10; i++) {
-    let junta = confereCaracteres();
-    let random = Math.floor(Math.random() * junta.length);
-    let randomCaracter = junta[random];
-    pass += randomCaracter;
+  if (checkToSymbols.checked) {
+    return symbols + latter;
   }
-  return pass;
+  if (checkToNumber.checked) {
+    return number + latter;
+  }
+  return latter;
 }
 
-console.log(gerarSenha());
+function createdPassword() {
+  let LimitedCaracteres =
+    inputLimitedCaracteres.value === ""
+      ? 8
+      : Number(inputLimitedCaracteres.value);
+
+  let pass = "";
+  for (let i = 0; i < LimitedCaracteres; i++) {
+    let caracters = checkCaracters();
+    let random = Math.floor(Math.random() * caracters.length);
+    let randomCaracter = caracters[random];
+    pass += randomCaracter;
+  }
+  password.textContent = pass;
+}
+
+function copyPassword() {
+  navigator.clipboard.writeText(password.innerText);
+}
